3dcb9aa0420fded11d5d6fcff5a2b1b272997c95
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-09-24 23:00:27 -0500

    amplify redo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { Amplify } from 'aws-amplify';
+import { getCurrentUser, signOut } from '@aws-amplify/auth';
+import awsconfig from './aws-exports.js';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import React, {useState, useEffect} from 'react';
+import '@aws-amplify/ui-react/styles.css';
 import logo from './logo.svg';
 import './App.css';
 
+Amplify.configure(awsconfig);
+
 function App() {
+  const [userInfo, setUserInfo] = useState({ username: '', userId: '', signInDetails: {} });
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = await getCurrentUser();
+        setUserInfo(user);
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
+    }
+    fetchUser();
+  }, []);
+
+  async function handleSignOut() {
+    await signOut();
+  }
+
+  const {myUser} = userInfo;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {}
         </p>
         <a
           className="App-link"
@@ -17,9 +44,10 @@ function App() {
         >
           Learn React
         </a>
+        <button type='button' onClick={handleSignOut}>Sign Out</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
