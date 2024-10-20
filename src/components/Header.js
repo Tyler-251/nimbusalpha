56046db62314c22fd233e8e47dba2695d6ec0610
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-10-20 12:12:28 -0500

    header link

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,13 @@ import React from "react";
 import "../styles/Header.css";
 import { signOut, fetchUserAttributes, getCurrentUser, fetchAuthSession } from '@aws-amplify/auth';
 
+const headerLeftStyle = {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "50%",
+    height: "100%",
+}
 
 function Header() {
     const [userInfo, setUserInfo] = React.useState('');
@@ -19,7 +26,10 @@ function Header() {
 
     return (
         <header>
-            <h1>Nimbus Calendar Manager</h1>
+            <div className="header-left" style={headerLeftStyle}>
+                <h1>Nimbus Calendar Manager</h1>
+                <a href="https://tylerrussell.me/nimbus" style={{"color":"white", "textDecoration": "none", "paddingLeft": "10px"}}>Learn About This Project Here</a>
+            </div>
             <div className="header-right">
                 <h2>Logged in as: {userInfo.email || "..."}</h2>
                 <button onClick={() => signOut()}>Sign Out</button>
